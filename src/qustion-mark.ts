2195d647f1478e849b3f30798b09e40c43df07ac
sajib689 <jibonraja11@gmail.com>
2024-10-11 23:17:46 +0600

    add nullable-unknown-never

---
+++ src/qustion-mark.ts
@@ -0,0 +1,9 @@
+const age: number = 22
+
+if(age >= 18) {
+    console.log('Yes')
+} else {
+    console.log('No')
+}
+const calculateTheAge = age === 18 ? 'Yes' : 'No'
+console.log(calculateTheAge)
