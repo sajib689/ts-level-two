49abf1b85b5cdf41838521ceba62ad02b835ef4e
sajib689 <jibonraja11@gmail.com>
2024-10-11 17:52:11 +0600

    rest parameter

--- src/function.ts
+++ src/function.ts
@@ -6,9 +6,22 @@ function add (num:number, num2:number) {
 const addArrow = (num:number, num2:number = 44) => {
     return num + num2
 }
-console.log(addArrow(44))
+
 const arr = [33, 444, 23]
 
 const newArray = arr.map((a: number) => a * a)
 
 add(23, 35)
+
+const arr1 = [44,45,46]
+const arr2 = [47, 48, 49]
+
+arr1.push(...arr2)
+
+// rest parameters
+
+const greetFriends = (friend1:string, friend2: string, friend3: string) => {
+    return friend1 + " " + friend2 + " " + friend3
+}
+const ok = greetFriends('Good Mornings', 'Good afternoon', 'Good night')
+console.log(ok)
