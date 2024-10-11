f3a4656179b91ed27bf4d7266182731baa91c75f
sajib689 <jibonraja11@gmail.com>
2024-10-11 17:44:35 +0600

    src file related issu solved

--- src/function.ts
+++ src/function.ts
@@ -3,10 +3,10 @@
 function add (num:number, num2:number) {
     return num + num2;
 }
-const addArrow = (num:number, num2:number) => {
+const addArrow = (num:number, num2:number = 44) => {
     return num + num2
 }
-
+console.log(addArrow(44))
 const arr = [33, 444, 23]
 
 const newArray = arr.map((a: number) => a * a)
