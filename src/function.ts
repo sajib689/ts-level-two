81a2ab6ed63910c6195a70404bf91e02d9e9a46a
sajib689 <jibonraja11@gmail.com>
2024-10-11 17:24:46 +0600

    add function with array mao

--- src/function.ts
+++ src/function.ts
@@ -6,4 +6,9 @@ function add (num:number, num2:number) {
 const addArrow = (num:number, num2:number) => {
     return num + num2
 }
+
+const arr = [33, 444, 23]
+
+const newArray = arr.map((a: number) => a * a)
+
 add(23, 35)
